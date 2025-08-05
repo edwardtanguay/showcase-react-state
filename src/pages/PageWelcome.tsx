d255c3ffb561bf87a6c1897b63a89375d4c19017
Edward Tanguay (windows) <edward@tanguay.info>
2025-08-05 15:53:59 +0200

    Make example 2 layout

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -5,10 +5,20 @@ export const PageWelcome = () => {
 
 	return (
 		<>
-			<h2>Example 1</h2>
-			<div className="flex gap-2 items-center">
-				<button onClick={() => setIsOnline(!isOnline)}>Change online status</button>
-				<p className="text-xl">User is {isOnline ? "online" : "offline"}.</p>
+			<div className="example">
+				<h2>Example 1</h2>
+				<div className="flex gap-2 items-center">
+					<button onClick={() => setIsOnline(!isOnline)}>Change online status</button>
+					<p className={`text-xl ${isOnline ? 'userIsOnline' : 'userIsOffline'}`}>User is {isOnline ? "online" : "offline"}.</p>
+				</div>
+			</div>
+			<div className="example">
+				<h2>Example 2</h2>
+				<div className="flex gap-2 items-center">
+				<p className="text-xl">Password:</p>
+				<input type="password" className="border border-slate-300 rounded-md px-2 py-1" />
+				<button>Login</button>
+				</div>
 			</div>
 		</>
 	)
