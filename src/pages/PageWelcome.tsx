944644210d48f38aa22e0035dbe38ac769160efc
Edward Tanguay (windows) <edward@tanguay.info>
2025-08-05 17:33:24 +0200

    Finish example 3

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -3,6 +3,7 @@ import { ChangeEvent, useState } from "react";
 export const PageWelcome = () => {
 	const [isOnline, setIsOnline] = useState(false);
 	const [thePassword, setThePassword] = useState("");
+	const [isFormDisabled, setIsFormDisabled] = useState(false);
 
 	const handleChangePassword = (event: ChangeEvent<HTMLInputElement>) => {
 		const value = event.target.value;
@@ -16,27 +17,39 @@ export const PageWelcome = () => {
 		}
 	};
 
+	const handleDisableForm = () => {
+		setIsFormDisabled(!isFormDisabled);
+	};
+
 	return (
 		<>
-			<div className="example">
-				<h2>Example 1</h2>
-				<div className="flex gap-2 items-center">
-					<button onClick={() => setIsOnline(!isOnline)}>Change online status</button>
-					<p className={`text-xl ${isOnline ? 'userIsOnline' : 'userIsOffline'}`}>User is {isOnline ? "online" : "offline"}.</p>
+			<div className={`${isFormDisabled ? 'formIsDisabled' : ''}`}>
+				<div className="example">
+					<h2>Example 1</h2>
+					<div className="flex gap-2 items-center">
+						<button onClick={() => setIsOnline(!isOnline)}>Change online status</button>
+						<p className={`text-xl ${isOnline ? 'userIsOnline' : 'userIsOffline'}`}>User is {isOnline ? "online" : "offline"}.</p>
+					</div>
+				</div>
+				<div className="example">
+					<h2>Example 2</h2>
+					<div className="flex gap-2 items-center">
+						{isOnline ? (
+							<p className="text-xl">Enjoy the site.</p>
+						) : (
+							<>
+								<p className="text-xl">Password:</p>
+								<input type="password" value={thePassword} className="border border-slate-300 rounded-md px-2 py-1" onChange={handleChangePassword} />
+								<button className={`${thePassword.length < 8 ? 'disabled' : ''}`} onClick={handleLoginButton} disabled={thePassword.length < 8}>Login</button>
+							</>
+						)}
+					</div>
 				</div>
 			</div>
 			<div className="example">
-				<h2>Example 2</h2>
+				<h2>Example 3</h2>
 				<div className="flex gap-2 items-center">
-					{isOnline ? (
-						<p className="text-xl">Enjoy the site.</p>
-					) : (
-						<>
-							<p className="text-xl">Password:</p>
-							<input type="password" value={thePassword} className="border border-slate-300 rounded-md px-2 py-1" onChange={handleChangePassword} />
-							<button className={`${thePassword.length < 8 ? 'disabled' : ''}`} onClick={handleLoginButton} disabled={thePassword.length < 8}>Login</button>
-						</>
-					)}
+					<input type="checkbox" id="disableForm" onClick={handleDisableForm} /> <label htmlFor="disableForm" className="text-xl">Disable form</label>
 				</div>
 			</div>
 		</>
